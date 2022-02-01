d59bf40af2e8613ed1936bb907e01f8ecb574f8f
stellalc7 <stellalc7@gmail.com>
2022-01-31 21:09:36 -0500

    fixing arc LOGIC

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -15,9 +15,11 @@ body.append(iframe);
 
 
 /*   ------------------------- GLOBE -------------------------   */
-const arcsData = [];
+let arcsData = [];
+let lons = [];
+let lats = [];
 // const labelsData = [];
-const myGlobe = Globe({ rendererConfig: {
+let myGlobe = Globe({ rendererConfig: {
                               autoclear: false,
                               powerPreference: "low-power",
                               // context: this
@@ -27,27 +29,27 @@ const myGlobe = Globe({ rendererConfig: {
 myGlobe(globeViz)
   .globeImageUrl('//unpkg.com/three-globe/example/img/earth-dark.jpg')
   .bumpImageUrl('//unpkg.com/three-globe/example/img/earth-topology.png')
-  .backgroundImageUrl('//unpkg.com/three-globe/example/img/night-sky.png')
+  // .backgroundImageUrl('//unpkg.com/three-globe/example/img/night-sky.png')
   .backgroundColor('black')
   .atmosphereColor('pink')
   (document.getElementById('globeViz'))
 
 
 /*   ------------------------- API -------------------------   */
-let lons, lats;
+
 let searchCity = document.getElementById('search-city');
 searchCity.addEventListener('submit', async function(e) {
   e.preventDefault();
   let city = searchCity.querySelector("input[type='text']").value.split(' ').join('%20');
   
-  // const urlStart = 'https://api.mixcloud.com/search';
-  // const url = `${urlStart}/?q=${city}&type=cloudcast`;
-  // fetch(url)
-  //   .then(response => response.json())
-  //   .then(data =>
-  //     iframe.src = 'https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=' + data.data[Math.floor(Math.random()*data.data.length)].url.slice(24)
-  //     )
-  //   .catch(error => console.log(error));
+  const urlStart = 'https://api.mixcloud.com/search';
+  const url = `${urlStart}/?q=${city}&type=cloudcast`;
+  fetch(url)
+    .then(response => response.json())
+    .then(data =>
+      iframe.src = 'https://www.mixcloud.com/widget/iframe/?hide_cover=1&mini=1&feed=' + data.data[Math.floor(Math.random()*data.data.length)].url.slice(24)
+      )
+    .catch(error => console.log(error));
 
     let coords = await fetch(`/api?searchCity=${encodeURIComponent(city)}`)
     .then(res => res.json()) // maybe don't need
@@ -58,41 +60,41 @@ searchCity.addEventListener('submit', async function(e) {
     lons.push(coords.lon)
     lats.push(coords.lat)
 
-  // arcsData.push({
-  //   startLat: (Math.random() - 0.5) * 180,
-  //   startLng: (Math.random() - 0.5) * 360,
-  //   endLat: (Math.random() - 0.5) * 180,
-  //   endLng: (Math.random() - 0.5) * 360,
-  //   color: [
-  //     ['red', 'pink', 'white', 'magenta'][Math.round(Math.random() * 3)],
-  //     ['red', 'pink', 'white', 'magenta'][Math.round(Math.random() * 3)]
-  //   ]
-  // });
+  arcsData.push({
+    startLat: lats[lats.length-2],
+    startLng: lons[lons.length-2],
+    endLat: lons[lons.length-1],
+    endLng: lons[lons.length-1],
+    color: [
+      ['red', 'pink', 'white', 'magenta'][Math.round(Math.random() * 3)],
+      ['red', 'pink', 'white', 'magenta'][Math.round(Math.random() * 3)]
+    ]
+  });
 
   // labelsData.push({
-  //   name: `${artist.split('%20').join(' ')}`,
+  //   name: `${city.split('%20').join(' ')}`,
   //   lat: arcsData[arcsData.length-1].startLat,
   //   lng: arcsData[arcsData.length-1].startLng
   // });
 
   // labelsData.push({
-  //   name: `${relatedArtist}`,
+  //   name: `${city}`,
   //   lat: arcsData[arcsData.length-1].endLat,
   //   lng: arcsData[arcsData.length-1].endLng
   // });
   
-  // myGlobe(globeViz)
-  //   .arcsData(arcsData)
-  //   // .labelsData(labelsData)
-  //   // .labelLat(d => d.lat)
-  //   // .labelLng(d => d.lng)
-  //   // .labelText(d => d.name)
-  //   // .labelSize(2)
-  //   // .labelDotRadius(0.75)
-  //   // .labelColor(() => 'pink')
-  //   // .onLabelHover(label, prevlabel)
-  //   .arcColor('color')
-  //   .arcDashGap(() => Math.random())
-  //   .arcDashAnimateTime(() => Math.random() * 4000 + 500)
-  //   (document.getElementById('globeViz'))
+  myGlobe(globeViz)
+    .arcsData(arcsData)
+    // .labelsData(labelsData)
+    // .labelLat(d => d.lat)
+    // .labelLng(d => d.lng)
+    // .labelText(d => d.name)
+    // .labelSize(2)
+    // .labelDotRadius(0.75)
+    // .labelColor(() => 'pink')
+    // .onLabelHover(label, prevlabel)
+    .arcColor('color')
+    .arcDashGap(() => Math.random())
+    .arcDashAnimateTime(() => Math.random() * 4000 + 500)
+    (document.getElementById('globeViz'))
 });
